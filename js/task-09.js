26986fdbe5168d61199ca6fcc7a1d6919575e592
Bohdan <102989300+Middasss@users.noreply.github.com>
2022-07-06 17:36:50 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,10 @@
 const body = document.querySelector('body');
 const spanEl = document.querySelector('.color');
-const btnEl = document.querySelector('.change-color');
 
 const btnColorChanger = () => {
   const getRandomHexColor = () =>`#${Math.floor(Math.random() * 16777215).toString(16)}`;
     
   spanEl.textContent = getRandomHexColor();
-  
   body.style.background = getRandomHexColor();
 }
 
